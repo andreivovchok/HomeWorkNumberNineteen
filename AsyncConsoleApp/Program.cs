7710c2c996f40974dd717e5c256051e7e2c51687
Andrei Vovchok <andreivovchok@bk.ru>
2021-09-25 19:13:35 +0300

    Добавил решение задания

---
+++ AsyncConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleApp
+{
+    class Program
+    {
+        public static void DoAction(string text, int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(text);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Do();
+            Console.ReadKey();
+        }
+
+        private static async void Do()
+        {
+            await Task.Run(() => DoAction("ff", 2));
+            Task.Run(() => DoAction("aa", 2));
+        }
+    }
+}
